aec9bbfc8c955b397432518ad42a36af59845d7d
mozix5 <mosinmd827@gmail.com>
2024-07-26 15:49:32 +0530

    remove padding

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} px-48`}>
+      <body className={`${inter.className}`}>
         <QueryClientProvider>
           <AuthProvider>
             <NavBar />
