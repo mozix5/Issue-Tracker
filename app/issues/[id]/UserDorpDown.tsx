736619f5946e3860856b61c9733d68465123c6a8
mozix5 <mosinmd827@gmail.com>
2024-07-30 20:21:59 +0530

    add user image

--- app/issues/[id]/UserDorpDown.tsx
+++ app/issues/[id]/UserDorpDown.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import {
   Select,
@@ -7,50 +7,65 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { useEffect, useState } from "react";
+import { cache, useEffect, useState } from "react";
 import axios from "axios";
 import { toast } from "sonner";
 import { Skeleton } from "@/components/ui/skeleton";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { FaUser } from "react-icons/fa";
 
 type UserDropDownProps = {
   assignedToUserId: string | null;
   issueId: string;
 };
 
+type UserOptions = {
+  id: string;
+  name: string;
+  image?: string;
+};
+
 const UserDropDown = ({ assignedToUserId, issueId }: UserDropDownProps) => {
-  const [userOptions, setUserOptions] = useState<{ id: string; label: string }[]>([]);
+  const [userOptions, setUserOptions] = useState<UserOptions[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const fetchUsers = async () => {
+    const fetchUsers = cache(async () => {
       try {
         const response = await axios.get("/api/users");
-        const users = response.data.map((user: { id: string; name: string }) => ({
+        const users = response.data.map((user: UserOptions) => ({
           id: user.id,
-          label: user.name || "Unnamed User",
+          name: user.name || "Unnamed User",
+          image: user.image,
         }));
-        setUserOptions([...users, { id: "unassigned", label: "Unassigned" }]);
+        setUserOptions([...users, { id: "unassigned", name: "Unassigned" }]);
       } catch (error) {
         toast.error("Failed to load users");
       } finally {
         setIsLoading(false);
       }
-    };
+    });
 
     fetchUsers();
   }, []);
 
   const assignUserToIssue = async (userId: string) => {
     try {
-      await axios.patch("/api/issues/" + issueId, { assignedToUserId: userId });
-      toast.success("User assigned successfully");
+      await axios.patch("/api/issues/" + issueId, {
+        assignedToUserId: userId === "unassigned" ? null : userId,
+      });
+      if (userId === "unassigned") {
+        toast.success("User unassigned successfully");
+      } else {
+        toast.success("User assigned successfully");
+      }
     } catch (error) {
       toast.error("User could not be assigned");
     }
   };
 
   if (isLoading) {
-    return <Skeleton className="h-10 bg-[#ececec]/10"/>;
+    return <Skeleton className="h-10 rounded-xl bg-[#ececec]/10" />;
   }
 
   return (
@@ -60,8 +75,21 @@ const UserDropDown = ({ assignedToUserId, issueId }: UserDropDownProps) => {
       </SelectTrigger>
       <SelectContent>
         {userOptions.map((option) => (
-          <SelectItem value={option.id} key={option.id} className="capitalize">
-            {option.label}
+          <SelectItem
+            value={option.id}
+            key={option.id}
+            className="capitalize px-2"
+          >
+            <div className="flex items-center gap-2">
+              <Avatar className="h-6 w-6">
+                <AvatarImage src={option.image} />
+                <AvatarFallback>
+                  <FaUser />
+                </AvatarFallback>
+              </Avatar>
+
+              {option.name}
+            </div>
           </SelectItem>
         ))}
       </SelectContent>
