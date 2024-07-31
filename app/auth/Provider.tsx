75b12bdc6474c4e0fcd7392680fc3690cd238b03
mozix5 <mosinmd827@gmail.com>
2024-07-31 14:59:39 +0530

    revert code

--- app/auth/Provider.tsx
+++ app/auth/Provider.tsx
@@ -1,47 +1,10 @@
 "use client";
 
-import { Session } from "next-auth";
-import {
-    SessionProvider as NextSessionProvider,
-    getSession
-} from "next-auth/react";
-import { usePathname } from "next/navigation";
-import {
-    ReactNode,
-    useCallback,
-    useEffect,
-    useState
-} from "react";
+import { SessionProvider } from "next-auth/react";
+import React, { PropsWithChildren } from "react";
 
-// Retrieve user session for the app's session context
-export default function SessionProvider({
-    children
-}: {
-    children: ReactNode;
-}) {
-    const [ session, setSession ] = useState<Session | null>(null);
-    const pathName = usePathname();
+const AuthProvider = ({ children }: PropsWithChildren) => {
+  return <SessionProvider>{children}</SessionProvider>;
+};
 
-    const fetchSession = useCallback(async () => {
-        try {
-            const sessionData = await getSession();
-            setSession(sessionData);
-        } catch (error) {
-            setSession(null);
-
-            if (process.env.NODE_ENV === "development") {
-                console.error(error);
-            }
-        }
-    }, []);
-
-    useEffect(() => {
-        fetchSession().finally();
-    }, [fetchSession, pathName]);
-
-    return (
-        <NextSessionProvider session={session}>
-            {children}
-        </NextSessionProvider>
-    );
-}
+export default AuthProvider;
