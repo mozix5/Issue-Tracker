bfb2f6703b8b95f7e63e5aebf651b0d76e196fcb
mozix5 <mosinmd827@gmail.com>
2024-08-05 19:59:52 +0530

    add check for session

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,12 +1,24 @@
 import dynamic from "next/dynamic";
 import IssueFormLoading from "./loading";
+import { getServerSession } from "next-auth";
+import AuthOptions from "@/app/auth/authOptions";
 
 const IssueForm = dynamic(() => import("@/app/components/form/IssueForm"), {
   ssr: false,
   loading: () => <IssueFormLoading />,
 });
 
-const CreateIssuePage = () => {
+const CreateIssuePage = async () => {
+  const session = await getServerSession(AuthOptions);
+
+  if (!session) {
+    return (
+      <div className="px-6 xl:px-48 md:px-16">
+        <p>You must be signed in to create an issue.</p>
+      </div>
+    );
+  }
+
   return (
     <div className="px-6 xl:px-48 md:px-16">
       <IssueForm />
